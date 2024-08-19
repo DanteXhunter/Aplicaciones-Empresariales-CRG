f32048e0a309ccd3d9a942e9c53048c600f95cc1
DanteXhunter <c.rojas9091@outlook.com>
2024-08-19 09:35:48 -0600

    Primer commit Practica 2

---
+++ practica2/js/index.js
@@ -0,0 +1,9 @@
+var miNumero = 0;
+miNumero = 10 + 20;
+miNumero = 2.5;
+console.log(miNumero);
+var booleano = true;
+booleano = false;
+var encendido;
+encendido = true;
+console.log(encendido);
---
+++ practica2/src/index.ts
@@ -0,0 +1,12 @@
+let miNumero:number = 0;
+
+miNumero = 10 + 20;
+miNumero = 2.5;
+console.log(miNumero);
+
+let booleano:boolean = true;
+booleano = false;
+
+let encendido:boolean;
+encendido = true;
+console.log(encendido);
