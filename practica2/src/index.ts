a896b373780b361b2b97ad29c4c32cfcfb02c362
DanteXhunter <c.rojas9091@outlook.com>
2024-08-20 08:39:49 -0600

    Practica 2 segundo commit

--- practica2/src/index.ts
+++ practica2/src/index.ts
@@ -9,4 +9,15 @@ booleano = false;
 
 let encendido:boolean;
 encendido = true;
-console.log(encendido);
+console.log("Encendido es:", encendido);
+console.log("Encendido es:", {encendido});
+
+encendido = true && false;
+console.log("Encendido es:", encendido);
+
+
+let normal:any = 'hola';
+normal=true;
+normal=10;
+normal=11.25;
+console.log("Normal es:", normal);
